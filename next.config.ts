8b0f21d8fe37949ab4a5a73d47152f0d94e3ebfc
Ismail424 <cicas03@hotmail.com>
2025-03-27 21:06:43 +0100

    Update Next.js configuration for Chakra UI optimization and adjust TypeScript settings

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    optimizePackageImports: ["@chakra-ui/react"],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
